a526db4d607700af7c14e8f0f3fa830b49cf21c2
DumpsterDave <35818070+DumpsterDave@users.noreply.github.com>
2021-01-25 09:54:27 -0600

    Azure Functions switched to Python3

--- py/controller.py
+++ py/controller.py
@@ -87,9 +87,9 @@ def OnKill(signum, frame):
 def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
   x_headers = 'x-ms-date:' + date
   string_to_hash = method + "\n" + str(content_length) + "\n" + content_type + "\n" + x_headers + "\n" + resource
-  bytes_to_hash = bytes(string_to_hash).encode('utf-8')  
+  bytes_to_hash = bytes(string_to_hash, encoding="utf-8")  
   decoded_key = base64.b64decode(shared_key)
-  encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest())
+  encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
   authorization = "SharedKey {}:{}".format(customer_id,encoded_hash)
   return authorization
 
@@ -100,15 +100,13 @@ def post_data(customer_id, shared_key, body, log_type):
   rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
   content_length = len(body)
   signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-  uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + resource + '?api-version=2016-04-01'
-
   headers = {
     'content-type': content_type,
     'Authorization': signature,
     'Log-Type': log_type,
     'x-ms-date': rfc1123date
   }
-
+  uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + resource + '?api-version=2016-04-01'
   return requests.post(uri,data=body, headers=headers)
 
 def WriteLog(message):
